ba77c982c4c3c208a7d94b8caea6081cf3001f4e
Arran Schlosberg <me@arranschlosberg.com>
2023-05-12 10:57:04 +0100

    Revert unnecessary changes to `raffle.sh`

--- raffle.sh
+++ raffle.sh
@@ -5,9 +5,9 @@ set -euo pipefail;
 function draw() {
     list="${1}"
     numToDraw="${2}"
-
     >&2 echo -e "\n================================================================================\n"
     >&2 echo -e "Drawing from ${list}\n"
+
     folder=$(dirname "${list}")
     entropy=$(cat "${folder}/entropy" | tail -n 1 | sed -n 's/^0x\([0-9a-fA-F]*\)$/\1/p')
 
