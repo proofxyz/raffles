5eb59c7d69549dc0bcf0b930ff6dcdf332cd4560
Forest Anderson <forest@forestanders.in>
2023-12-22 13:01:10 -0500

    Take 2
    The previous winner already won so we're running it again

--- raffle.sh
+++ raffle.sh
@@ -80,6 +80,7 @@ draw toobins/nov-15/participants 1
 draw toobins/dec-02/participants 3
 draw toobins/dec-14/participants 2
 draw toobins/dec-22/participants 1
+draw toobins/dec-22-take-2/participants 1
 
 draw grails/season-03-deafbeef-physical/cauldron 1
 draw grails/season-03-deafbeef-physical/bronze 1
