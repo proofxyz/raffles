582eacaa622f886a7fe646c20fa044dae588b6a6
Forest Anderson <forest@forestanders.in>
2023-12-22 10:48:20 -0500

    Setting up the raffle for today (#84)
    * Adding block and participants

--- raffle.sh
+++ raffle.sh
@@ -79,6 +79,7 @@ draw toobins/nov-08/participants 1
 draw toobins/nov-15/participants 1
 draw toobins/dec-02/participants 3
 draw toobins/dec-14/participants 2
+draw toobins/dec-22/participants 1
 
 draw grails/season-03-deafbeef-physical/cauldron 1
 draw grails/season-03-deafbeef-physical/bronze 1
@@ -96,6 +97,7 @@ draw notes-from-a-neutron-star-exhibition/receive-transmission/receive-transmiss
 
 draw moonbirds/nov-06/participants 1
 draw grails/season-05/full-set-holders 10
+
 
 draw grails/season-05-divergence-mint/projectIDs 18 $i
 
